9e14a96b0c04bb49803df968f22a77317a922d81
khamushu <khamushu@khamushus-MacBook-Air.local>
2025-11-08 17:01:55 +0700

    fix jenkins warning threshold

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from Dockerized Node.js on AWS! this is CICD PROJECT nice'));
+app.get('/', (req, res) => res.send('Hello from Dockerized Node.js on AWS! this is CICD PROJECT!'));
 app.listen(3000, () => console.log('Server running on port 3000'));
